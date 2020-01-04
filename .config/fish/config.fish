d0367ce8ccffea769030100fa3e4ee01cf992881
Fyodor Perejoguine <fyodordev@gmail.com>
2020-01-04 17:03:44 +0100

    New, more minimalist setup (no blur).
    Signed-off-by: Fyodor Perejoguine <fyodordev@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,6 @@
 #!/bin/fish
+
+source ~/.profile
 # Set the cursor shapes for the different vi modes.
 # Doesn't work for some reason.
 set fish_cursor_default     underscore      blink
@@ -8,3 +10,4 @@ set fish_cursor_visual      underscore blink
 
 alias dfgit='/usr/bin/git --git-dir=$HOME/.cfg-git/ --work-tree=$HOME'
 alias vi='/usr/bin/nvim'
+
--- .profile
+++ .profile
@@ -1,2 +1,5 @@
 export EDITOR=/usr/bin/nvim
+export VISUAL=/usr/bin/nvim
 export BROWSER=/usr/bin/chromium
+
+set PATH $PATH:$HOME/.scripts
