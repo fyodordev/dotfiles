8b75f9da3cbe725db43e7ac35f2b60800ec7ebb6
Fyodor Perejoguine <fyodordev@gmail.com>
2020-01-14 10:42:13 +0100

    Make fish shell change cursor on vi mode work.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@
 fenv source ~/.profile
 # Set the cursor shapes for the different vi modes.
 # Doesn't work for some reason.
-set fish_cursor_default     underscore      blink
+set fish_cursor_default     block      blink
 set fish_cursor_insert      line       blink
 set fish_cursor_replace_one underscore blink
 set fish_cursor_visual      underscore blink 
