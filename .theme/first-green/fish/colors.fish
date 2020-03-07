366373768586c5987d499192c9811122c36a2c44
Fyodor Perejoguine <fyodordev@gmail.com>
2020-03-07 17:02:45 +0100

    Change to theme switching system with symlinks.

--- .theme/first-green/fish/colors.fish
+++ .theme/first-green/fish/colors.fish
@@ -1,21 +1,52 @@
 #!/bin/fish
 
-fenv source ~/.profile
-source ~/.config/fish/colors.fish
-# Set the cursor shapes for the different vi modes.
-# Doesn't work for some reason.
-set fish_cursor_default     block      blink
-set fish_cursor_insert      line       blink
-set fish_cursor_replace_one underscore blink
-set fish_cursor_visual      underscore blink 
+# Terminal colors
+set foreground dddddd
+set background 062C30 
+
+set grey_darkest 282a2e
+set grey_dark 373b41 
+set grey_light 707880
+set grey_lightest c5c8c6
+set red a54242
+set red_light cc6666
+set green 8c9440
+set green_light b5bd68
+set yellow de935f
+set yellow_light f0c674
+set blue 5f819d
+set blue_light 81a2be
+set magenta 85678f
+set magenta_light b294bb
+set cyan 5E8d87
+set cyan_light 8abeb7
 
 
 # Set colors
 # Refer to doc here: https://fishshell.com/docs/current/index.html#variables-color
 # The following variables are available to change the highlighting colors in fish:
+#
+set fish_color_prompt_primary FFFFFF
 
-# set fish_color_normal 000000 # the default color
-# fish_color_command 000000 # the color for commands
+#set fish_color_prompt_secondary 5fdfff # Used for git for instance
+set fish_color_prompt_accent 87ff00
+set fish_color_prompt_timestamp FFFFFF
+
+#set -l white (set_color FFFFFF)
+#cyan (set_color 5fdfff)
+#orange (set_color df5f00)
+#hotpink (set_color df005f)
+#blue (set_color 53D1ED)
+#green (set_color 87ff00)
+#greeno (set_color -o 87ff00)
+#purple (set_color af5fff)
+#red (set_color FF0000)
+#redo (set_color -o FF0000)
+#yellow (set_color E0D757)
+
+
+#set fish_color_normal 000000 # the default color
+set fish_color_command FFFFFF # the color for commands
 # fish_color_quote 000000 # the color for quoted blocks of text
 # fish_color_redirection 000000 # the color for IO redirections
 # fish_color_end 000000 # the color for process separators like ';' and '&'
@@ -28,7 +59,7 @@ set fish_cursor_visual      underscore blink
 # fish_color_operator 000000 # the color for parameter expansion operators like '*' and '~'
 # fish_color_escape 000000 # the color used to highlight character escapes like '\n' and '\x70'
 # fish_color_cwd 000000 # the color used for the current working directory in the default prompt
-# fish_color_autosuggestion 000000 # the color used for autosuggestions
+# fish_color_autosuggestion 000000 # the color used for autosuggestions (also timestamp right of prompt)
 # fish_color_user 000000 # the color used to print the current username in some of fish default prompts
 # fish_color_host 000000 # the color used to print the current host system in some of fish default prompts
 # fish_color_cancel 000000 # the color for the '^C' indicator on a canceled command
@@ -39,11 +70,4 @@ set fish_cursor_visual      underscore blink
 # fish_pager_color_description 000000 # the color of the completion description
 # fish_pager_color_progress 000000 # the color of the progress bar at the bottom left corner
 # fish_pager_color_secondary 000000 # the background color of the every second completion
-
-
-alias dfgit='/usr/bin/git --git-dir=$HOME/.cfg-git/ --work-tree=$HOME'
-alias vi='/usr/bin/nvim'
-alias sxiv='sxiv -b'
-alias rng='ranger'
-alias cli='xclip -sel clip'
 
