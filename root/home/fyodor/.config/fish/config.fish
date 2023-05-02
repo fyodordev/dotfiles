befd2f8cac6179133f1e7ae1b366f91a28619768
Fyodor Perejoguine <fyodordev@gmail.com>
2023-05-02 16:56:29 +0200

    Change fish cursor.

--- root/home/fyodor/.config/fish/config.fish
+++ root/home/fyodor/.config/fish/config.fish
@@ -174,6 +174,6 @@ end
 fish_vi_key_bindings
 
 set fish_cursor_default     block      blink
-set fish_cursor_insert      line       blink
+set fish_cursor_insert      underscore blink
 set fish_cursor_replace_one underscore blink
 set fish_cursor_visual      underscore blink
